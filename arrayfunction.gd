8fd972f65ccd08e1c4062b08abc29aeae7841645
Eliot <76494519+EliotW3@users.noreply.github.com>
2025-03-16 13:56:03 +0000

    Added count if num function
    Needs testing, typed out on phone.

--- arrayfunction.gd
+++ arrayfunction.gd
@@ -33,3 +33,36 @@ static func product(numbers : Array) -> float:
 			total = total * numbers[i]
 	
 	return total
+
+static func count_if_num(numbers : Array, type : int, criteria : float) -> int:
+	# take array as input, "type" represents ><= or 4 for all numbers, compare against criteria
+	
+	var count = 0
+	
+	if type == 4:
+		for i in range(0,numbers.size()):
+			if typeof(numbers[i]) == 2 or typeof(numbers[i]) == 3:
+				count += 1
+	else:
+		# handle criterias
+		if type == 1:
+			# less than criteria
+			for i in range(0,numbers.size()):
+				if typeof(numbers[i]) == 2 or typeof(numbers[i]) == 3:
+					if float(numbers[i]) < criteria:
+						count += 1
+		elif type == 2:
+			# equal to criteria
+			for i in range(0,numbers.size()):
+				if typeof(numbers[i]) == 2 or typeof(numbers[i]) == 3:
+					if is_equal_approx(float(numbers[i]), criteria):
+						count += 1
+		elif type == 3:
+			# more than criteria
+			for i in range(0,numbers.size()):
+				if typeof(numbers[i]) == 2 or typeof(numbers[i]) == 3:
+					if float(numbers[i]) > criteria:
+						count += 1
+
+	return count
+	
